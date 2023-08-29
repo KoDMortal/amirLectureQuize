febafb8081c56c1b371365a12b88ea06a575ca39
KoDMortal <kodmortal999@gmail.com>
2023-08-29 13:28:20 +0300

    Made mathInput.fy

---
+++ mathInput.py
@@ -0,0 +1,5 @@
+""" Aleksandr Chaban TXL23S1-A """
+acceleration=float(input("Please enter the value of acceleration due to gravity (in m/s^2)"))
+mass=float(input("Please enter the mass of an object (in kg)"))
+weight=mass*acceleration
+print(weight)
